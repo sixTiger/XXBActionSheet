77b28748cdbdf742c4bd3aceaaa658cd6225646b
杨小兵 <six_tiger@163.com>
2015-08-17 00:14:18 +0800

    优化代码

--- XXBActionSheetView_table/XXBActionSheetView_table/ViewController.m
+++ XXBActionSheetView_table/XXBActionSheetView_table/ViewController.m
@@ -16,7 +16,7 @@
 @implementation ViewController
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    // 1.创建HJCActionSheet对象, 可以随意设置标题个数，第一个为取消按钮的标题，需要设置代理才能监听点击结果
+    // 1.创建XXBActionSheet对象, 可以随意设置标题个数，第一个为取消按钮的标题，需要设置代理才能监听点击结果
     XXBActionSheet *sheet = [[XXBActionSheet alloc] initWithTitle:@"title" delegate:self cancelButtonTitle:@"cancale" otherButtonTitles:@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",nil];
     // 2.显示出来
     [sheet showInView:self.view];
